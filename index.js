4694069395cd91096fff23426ce4dba0a565d262
Beatriz Rogers <beatrizrogers55@gmail.com>
2025-07-12 17:30:25 -0300

    Implementação do Shader de efeito das nuvens

--- index.js
+++ index.js
@@ -77,6 +77,130 @@ let jumpSpeed = 0.05;
 let maxJumpHeight = 12;
 let originalY = ovelha.position.y;
 
+
+// Shaders
+const geometriaShader = new THREE.PlaneGeometry( w/100, h/100);
+const materialShaderNuvens = new THREE.RawShaderMaterial({
+    transparent: true,
+    depthWrite: false,
+    uniforms: {
+        uTime: { value: 0.0 }
+    },
+    vertexShader: `
+    precision mediump float;
+    precision mediump int;
+
+    attribute vec3 position;
+    attribute vec3 normal;
+    attribute vec2 uv;
+
+    uniform mat4 modelViewMatrix;
+    uniform mat4 projectionMatrix;
+    uniform float uTime;
+
+    varying vec3 vPosition;
+    varying vec3 vNormal;
+    varying vec2 vUv;
+
+    void main() {
+        vPosition = position;
+        vNormal = normal;
+        vUv = uv;
+
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+    `,
+    fragmentShader: `
+    precision mediump float;
+    precision mediump int;
+
+    uniform float uTime;
+
+    varying vec3 vPosition;
+    varying vec3 vNormal;
+    varying vec2 vUv;
+
+
+    // Código original de Darko Supe (omegasbk)
+    // Modificado e adaptado para GLSL por Beatriz Rogers
+
+    float noise(vec2 uv)
+    {
+        return fract(sin(uv.x * 113.0 + uv.y * 412.0) * 6339.0);
+    }
+
+    vec3 noiseSmooth(vec2 uv)
+    {
+        vec2 index = floor(uv);
+        vec2 pq = fract(uv);
+
+        pq = smoothstep(0.0, 1.0, pq);
+        
+        float topLeft = noise(index);
+        float topRight = noise(index + vec2(1.0, 0.0));
+        float top = mix(topLeft, topRight, pq.x);
+        
+        float bottomLeft = noise(index + vec2(0, 1));
+        float bottomRight = noise(index + vec2(1, 1));
+        float bottom = mix(bottomLeft, bottomRight, pq.x);
+        
+        return vec3(mix(top, bottom, pq.y));
+    }
+
+    float drawSmoothCircle(vec2 position, vec2 center, float maxRadius, float minRadius) {
+        return  smoothstep(minRadius, maxRadius, distance(position,center));
+    }
+
+    void main() {
+
+        vec2 uv = vUv;
+        
+        uv.x += uTime / 40.0;
+        uv.y += uTime / 40.0;
+        
+        vec2 uv2 = uv;
+        uv2.x += uTime / 10.0;
+        uv2.y -= uTime / 10.0;
+        
+        vec2 uv3 = uv;
+        uv3.x += uTime / 30.0;
+        uv3.y -= uTime / 30.0;
+            
+        vec3 col = noiseSmooth(uv * 4.);
+        
+        col += noiseSmooth(uv * 8.0) * 0.5;
+        col += noiseSmooth(uv2 * 16.0) * 0.25;
+        col += noiseSmooth(uv3 * 32.0) * 0.125;
+        col += noiseSmooth(uv3 * 64.0) * 0.0625;
+        
+        col /= 2.0;   
+        
+        col *= smoothstep(0.2, 0.6, col);   
+        
+        col = mix(1.0 - (col / 7.0), vec3(0), 1.0 - col);    
+        
+        float area = drawSmoothCircle(vUv, vec2(0.5), 0.7, 0.15);
+        float opacity = area - ((col.x + col.y + col.z) / 3.0);
+
+        // Output to screen
+        gl_FragColor = vec4(vec3(area) + col,opacity);
+    }
+    `,
+});
+
+const shaderMeshNuvens1 = new THREE.Mesh(geometriaShader, materialShaderNuvens);
+shaderMeshNuvens1.position.set(0, 26, 26);
+shaderMeshNuvens1.lookAt(0,30,30);
+
+const shaderMeshNuvens2 = new THREE.Mesh(geometriaShader, materialShaderNuvens);
+shaderMeshNuvens2.position.set(0, 0, 45);
+shaderMeshNuvens2.lookAt(0,0,50);
+
+scene.add(shaderMeshNuvens1);
+scene.add(shaderMeshNuvens2);
+
+const clock = new THREE.Clock();
+
 function animate() {
     invis.rotateY(-0.004);
 
@@ -131,6 +255,10 @@ function animate() {
             stayAir = false;
         }
     }
+
+    // Atualização dos Shaders por frame
+    const elapsedTime = clock.getElapsedTime();
+    materialShaderNuvens.uniforms.uTime.value = elapsedTime;
 
     requestAnimationFrame(animate);
     renderer.render( scene, camera_atual );
